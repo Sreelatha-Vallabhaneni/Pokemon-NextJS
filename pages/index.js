9b89e049458d0d38e603002ff42210c8fade8adc
sreelatha <sv@stromworks.com>
2022-05-15 03:33:05 +0200

    updated styling of pokemon list and single page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import GetPokemons from '../components/GetPokemons'
-import Layout from '../components/Layout'
+import React, { useState } from 'react';
+import GetPokemons from '../components/GetPokemons';
+import Layout from '../components/Layout';
+
 
 export default function Home({initialPokemon, allPokemons}) {
-  console.log('all SSR', initialPokemon, allPokemons);
-  
   return (
     <Layout title={'Pokemon'} >
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10">
@@ -16,10 +15,9 @@ export default function Home({initialPokemon, allPokemons}) {
   )
 }
 
-
 export async function getServerSideProps(context) {
   try {
-    const initialPokemon = await fetch('https://pokeapi.co/api/v2/pokemon').then((response) => response.json());
+    const initialPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`).then((response) => response.json());
 
     if (!initialPokemon) {
       return { notFound: true };
@@ -30,9 +28,7 @@ export async function getServerSideProps(context) {
         fetch(result.url).then((response) => response.json())
       )
     );
-
     const props = { initialPokemon, allPokemons };
-
     return { props };
   } catch (error) {
     console.error('runtime error: ', error);
--- pages/pokemon/[name].js
+++ pages/pokemon/[name].js
@@ -1,12 +1,32 @@
 import Image from 'next/image';
 import React from 'react'
 import Layout from '../../components/Layout';
+import { useRouter } from 'next/router'
 
 function Pokemon({pokemon}) {
+  const router = useRouter()
+  
   return (
-    <Layout title={'Pokemon'}>   
-        <p className="text-lg text-fuchsia-400">{pokemon.name}</p>
+    <Layout title={'Pokemon'}> 
+    <button type="button" onClick={() => router.push('/')} className="text-2xl tracking-wide bg-slate-900 text-white px-5 py-2">Back</button> 
+      <div className="flex items-center justify-center"> 
         <Image src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} width={400} height={400} layout="intrinsic" />
+        <div className=" text-orange-400">
+          <p className="text-4xl text-fuchsia-400 tracking tracking-wider uppercase font-mono leading-6">{pokemon.name}</p>
+          <div className="flex tracking-wider font-sans my-5 text-xl">
+            <div className="">
+              <h6>Weight</h6>
+              <h6>Height</h6>
+              <h6>Ability</h6>
+            </div>
+            <div className=" ml-5">
+              <p className="">{pokemon.weight}</p>
+              <p className="">{pokemon.height}</p>
+              <p className="">{pokemon.abilities[0].ability.name}</p>
+            </div>
+          </div>
+          </div>
+      </div>
     </Layout>
   )
 }
